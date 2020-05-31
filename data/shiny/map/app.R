b3fcc15f5af14ee2bc33d1abf3f3ed12506a8721
newcanopies <65022111+newcanopies@users.noreply.github.com>
2020-05-31 17:29:19 +0200

    i

--- data/shiny/map/app.R
+++ data/shiny/map/app.R
@@ -9,25 +9,47 @@ library(sf) #install.packages('sf')
 brexit <- st_read("data/britain_eu_ref.shp")
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- fixedPage(
   # many layout options are possible: https://shiny.rstudio.com/articles/layout-guide.html
   
-  # Application title
-  titlePanel("UK 2016 EU referendum results"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      # other button types: https://shiny.rstudio.com/tutorial/written-tutorial/lesson3/
-      radioButtons("radio", h3("Display value:"), choices = list("Turnout %" = "Pct_Turnou", "Remain %" = "Pct_Remain", "Leave %" = "Pct_Leave"), selected = "Pct_Turnou"),
-      plotOutput("valueHist", height = "400px")
-    ),
-  
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("brexitPlot", height="600px")
+  fixedRow(
+      column(12,
+      # Application title
+      titlePanel("UK 2016 EU referendum results"),
+            
+        fixedRow(
+          column(12,
+            # Sidebar with a slider input for number of bins 
+            sidebarLayout(
+            sidebarPanel(
+            # other button types: https://shiny.rstudio.com/tutorial/written-tutorial/lesson3/
+            radioButtons("radio", h3("Display value:"), choices = list("Turnout %" = "Pct_Turnou", "Remain %" = "Pct_Remain", "Leave %" = "Pct_Leave"), selected = "Pct_Turnou"),
+            plotOutput("valueHist", height = "400px"),
+            
+              fixedRow(
+               
+                      
+                column(10,
+                  ## <a href="https://newcanopies.github.io/hellowebgis/website/index.html">newcanopies</a> 
+                  "Back to", tags$a(href="https://newcanopies.github.io/hellowebgis/website/index.html", "newcanopies"),
+              )
+             
+          )
+        ),
+          column(width = 12,
+            # Show a plot of the generated distribution
+            mainPanel(
+              plotOutput("brexitPlot", height="600px")
+            )
+          )
+        )
+      )
     )
   )
+)
+)
+sidebarPanel(
+tags$a(href="https://newcanopies.github.io/hellowebgis/website/index.html", "newcanopies")
 )
 
 # Define server logic required to draw a histogram
